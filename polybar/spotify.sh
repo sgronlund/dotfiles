b6b690d20602bee7f8058b367139a30e62fc9870
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-03-10 18:43:17 +0100

    added player to be spotify in playerctl calls

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -2,18 +2,19 @@
 #
 # Simple script which uses `playerctl` to fetch metadata about the player playing,
 
+
 # Check if anything is playing
-active=$(playerctl status)
+active=$(playerctl status --player=spotify)
+
 
 if [[ $active == "Playing" ]] || [[ $active == "Paused" ]]; then
-  
   #TODO: shorten titles if length exceeds n characters
-  artist=$(playerctl metadata artist)
-  song=$(playerctl metadata title)
+  artist=$(playerctl metadata artist --player=spotify)
+  song=$(playerctl metadata title --player=spotify)
   
   # no artist, podcast playing --> album is artist
   if [[ $artist  == "" ]]; then
-    artist=$(playerctl metadata album)
+    artist=$(playerctl metadata album --player=spotify)
   fi
   echo "$song - $artist"
 else
