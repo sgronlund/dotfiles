6d25d7b24ed02371b63254f601ebe63194ffc953
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-03-12 19:08:49 +0100

    added check for handling long song names
    Quite simple fix which just truncates the song title with ... if it
    exceeds a certain amount of characters

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 #
 # Simple script which uses `playerctl` to fetch metadata about the player playing,
 
@@ -8,15 +8,20 @@ active=$(playerctl status -s --player=spotify)
 
 
 if [[ $active == "Playing" ]] || [[ $active == "Paused" ]]; then
-  #TODO: shorten titles if length exceeds n characters
   artist=$(playerctl metadata artist --player=spotify)
   song=$(playerctl metadata title --player=spotify)
   
+  # Handle long song names, maybe handle artist too?
+  max_length=35
+  if [[ ${#song} -gt $max_length ]]; then
+    song="${song:0:$max_length}..."
+  fi
+   
   # no artist, podcast playing --> album is artist
   if [[ $artist  == "" ]]; then
     artist=$(playerctl metadata album --player=spotify)
   fi
   echo "$song - $artist"
 else
-  echo "" #couldn't find a pauser or playing player 
+  echo "" #couldn't find a paused or playing player 
 fi
