92c1261d0d7b34f540788fd4102d654128984885
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-04-16 22:00:44 +0200

    small fix

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -3,6 +3,7 @@
 # Simple scripts to ease configuration of monitors
 
 arg=$1
+xrandr --auto 
 monitors=$(xrandr --listmonitors | awk '{print $4}')
 for i in $monitors
 do
@@ -12,20 +13,6 @@ do
 		ext=$i
 	fi
 done
-# Check if monitor is off then enable all and get name
-if [[ -z "$lap" ]]; then
-	xrandr --auto
-	monitors=$(xrandr --listmonitors | awk '{print $4}')
-	for i in $monitors
-	do
-		if [[ $i == *"eDP"* ]]; then
-			lap=$i
-		elif [[ $i == *"HDMI"* ]]; then
-			ext=$i
-		fi
-	done
-
-fi
 case $arg in
 	1)
 		xrandr --output "$ext" --auto --above "$lap" --auto
