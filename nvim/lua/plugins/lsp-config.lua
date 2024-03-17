0794a884d28c8a8e04cae664d0bbe5928206d270
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2024-03-18 00:10:39 +0100

    nvim: add conf

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "dockerls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.lua_ls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilites,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
