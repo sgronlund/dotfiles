d35b4c432ad5a334d25d2cb8fdaaef4cfdf4088a
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2024-05-12 22:13:52 +0200

    dwm: appearance
    switching border sizes and such :shrug:

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Iosevka Nerd Font:size=12" };
+static const char dmenufont[]       = "Iosevka Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -40,8 +40,8 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 2;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
@@ -67,9 +67,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *shifttermcmd[]  = { "alacritty", "--class", "floating_alacritty", NULL };
-static const char *nnncmd[]  = { "alacritt", "-e", "nnn -denA -T t", NULL };
-static const char *powermenucmd[]  = { "~/.config/rofi/powermenu/type-1/powermenu.sh", NULL };
+static const char *shifttermcmd[]  = { "alacritty --class floating_alacritty", NULL };
+static const char *nnncmd[]  = { "alacritty -e nnn -denA -T t", NULL };
+static const char *powermenucmd[]  = { "/home/frost/.config/rofi/powermenu/type-1/powermenu.sh", NULL };
 static const char *maimcmd[]  = { "maim -s | xclip -selection clipboard -t image/png", NULL };
 
 static const Key keys[] = {
@@ -77,7 +77,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_l,      spawn,          {.v = powermenucmd } },
 	{ MODKEY,                       XK_o,      spawn,          {.v = nnncmd } },
-	{ MODKEY,                       XK_x,      spawn,          {.v = maimcmd } },
+	{ MODKEY|ShiftMask,             XK_x,      spawn,          {.v = maimcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = shifttermcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Up,      focusstack,     {.i = +1 } },
