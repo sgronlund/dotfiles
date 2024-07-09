42c4f928a19dd63407df2220953f124fde0755f2
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2024-07-09 14:01:03 +0200

    misc: synd vim conf wsl linux

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,5 +4,6 @@ return {
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<leader><leader>', builtin.buffers, {})
 	end
 }
