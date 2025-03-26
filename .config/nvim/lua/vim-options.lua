f122a4a3f6acb8d24d3eaf01bf3fc1bca956dcb7
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2025-03-26 14:40:57 +0100

    nvim: add cmd alias for wq
    sometimes, due to being fast or clumsy, I tend to write wQ or Wq or W
    etc. this makes these variants behave as just writing wq or w!

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -20,3 +20,9 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<Tab>", ">>")
 vim.keymap.set("n", "<S-Tab>", "<<")
+-- clumsy bindings due to shift hehe
+vim.cmd([[cnoreabbrev W w
+cnoreabbrev Wq wq
+cnoreabbrev WQ wq
+cnoreabbrev wQ wq
+]])
