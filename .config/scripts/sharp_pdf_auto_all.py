55fd4a9c1828670c7486b7926bf1e428f648bac8
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2025-03-28 16:31:43 +0100

    scripts: add error handling for eom-script
    also included some comments in regards to where stuff can go wrong :)

--- .config/scripts/sharp_pdf_auto_all.py
+++ .config/scripts/sharp_pdf_auto_all.py
@@ -15,22 +15,40 @@ def main():
         year = f"{d_year-2000}"
     kassaslut = glob.glob(f"Kassaslut_{year}{month}*.pdf")
     varuspec = glob.glob(f"varuspecefikation_{year}{month}*.pdf")
-    assert(len(kassaslut) == len(varuspec))
+    # skip duplicate files
+    kassaslut = ([x for x in kassaslut if not re.search(r"\([0-9]+\)", x)])
+    varuspec = ([x for x in varuspec if not re.search(r"\([0-9]+\)", x)])
+    # some files have been removed, need to sort them in order by date
+    kassaslut.sort()
+    varuspec.sort()
+    # do we have equal amounts of items?
+    assert len(kassaslut) == len(varuspec), f"Number of files did not match {kassaslut}{varuspec}"
     items = zip(kassaslut,varuspec)
     for item in items:
         k = item[0]
         v = item[1]
-        reader = pypdf.PdfReader(k)
-        actual_pos = reader.pages[0].extract_text()[10:20]
+        print(f"Trying to merge {k} and {v}" )
+        reader_k = pypdf.PdfReader(k)
+        reader_v = pypdf.PdfReader(v)
+        v_date_parts = reader_v.pages[0].extract_text()[150:240].split("\n")[1] # FIXME: This bad boy may fail horribly for some user within the POS, but tried a short user (i.e. 4 chars) and a long user (12 chars)
+        actual_pos_v_start = (v_date_parts.split(" ")[3])
+        actual_pos_v_end = (v_date_parts.split(" ")[5])
+        assert actual_pos_v_start == actual_pos_v_end, f"Wrong file {v}! This is a file for weekly summaries"
+        actual_pos_k = reader_k.pages[0].extract_text()[10:20]
         try:
-            actual_pos_parsed = datetime.datetime.strptime(actual_pos, "%Y-%m-%d")
+            actual_pos_k_parsed = datetime.datetime.strptime(actual_pos_k, "%Y-%m-%d")
+            actual_pos_v_parsed = datetime.datetime.strptime(actual_pos_v_start, "%Y-%m-%d")
         except:
             continue
+        assert actual_pos_v_parsed == actual_pos_k_parsed, f"Files did not have matching dates ({k}, {v})"
+        reader_k.close()
+        reader_v.close()
         merger = pypdf.PdfWriter()
         merger.append(k)
         merger.append(v)
-        merger.write(f"Sharprapport - {actual_pos_parsed.strftime('%y%m%d')} Sammanslagen.pdf")
+        merger.write(f"Sharprapport - {actual_pos_k_parsed.strftime('%y%m%d')} Sammanslagen.pdf")
         merger.close()
+        print(f"Merged {k} and {v}")
 
 if __name__ == "__main__":
     main()
