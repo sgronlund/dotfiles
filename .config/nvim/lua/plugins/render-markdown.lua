26fb39f9b9b989edc6da944cb1c0366f4ef9f998
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2025-05-21 20:28:34 +0200

    nvim: add plugin for render markdown
    essentially just used for prettier tables when showing a document

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    config = function ()
+       vim.keymap.set("n","<leader>rm",":RenderMarkdown buf_toggle <CR>", {})
+    end
+}
