7e4f6b0725f3b0b4aaa4f2fddd0de6f29e6ada34
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2025-08-27 12:38:40 +0200

    nvim: add plugins for usage of R

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 			local config = require("nvim-treesitter.configs")
 			config.setup({
 				auto_install = true,
-				ensure_installed = { "c", "glsl", "vim", "lua", "python" },
+				ensure_installed = { "c", "glsl", "vim", "lua", "python" , "r"},
 				highlight = { enable = true },
 				indent = { enable = true },
 			})
