6b844b5198f5461db2348232f7bf9359c38aae96
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2025-09-05 09:14:04 +0200

    nvim: minor tweaks

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 			local config = require("nvim-treesitter.configs")
 			config.setup({
 				auto_install = true,
-				ensure_installed = { "c", "glsl", "vim", "lua", "python" , "r"},
+				ensure_installed = { "c", "glsl", "vim", "lua", "python" , "r", "markdown", "markdown_inline", "latex"},
 				highlight = { enable = true },
 				indent = { enable = true },
 			})
