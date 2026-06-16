afc9ab8a89ab4b6c5b287fdd65b658336d4d782d
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2026-06-16 13:21:31 +0200

    script (monitor): fix primary and other things
    the flag --listmonitors was not acting as I expected possibly could have
    used --listactivemonitors but this awk-command does the job.

--- .config/scripts/monitor.sh
+++ .config/scripts/monitor.sh
@@ -2,34 +2,30 @@
 # Simple scripts to ease configuration of monitors
 
 arg=$1
-xrandr --auto 
-monitors=$(xrandr --listmonitors | awk '{print $4}')
-for i in $monitors
-do
-	if [[ $i == *"eDP"* ]]; then
-		lap=$i
-	elif [[ $i == *"HDMI"* ]]; then
-		ext=$i
-	fi
+monitors=($(xrandr | awk '/ connected/ {print $1}'))
+for i in "${monitors[@]}"; do
+    if [[ $i == *"eDP"* ]]; then
+        lap=$i
+    elif [[ $i == *"HDMI"* ]]; then
+        ext=$i
+    fi
 done
 case $arg in
-	1)
-		xrandr --output "$ext" --primary --auto --right-of "$lap"
-        if [[ -n "$ext" ]]; then
-            xrandr --output "$ext" --primary --auto --right-of "$lap"
-        else 
-
-		xrandr --output "$lap" --primary --auto 
-        fi
-
-		;;
-	2)
-		xrandr --output "$lap" --off 
-		;;
-	3)
-		xrandr --output "$ext" --scale 1 --same-as "$lap" 
-		;;
-	*)
-		;;
+1)
+    if [[ -n "$ext" ]]; then
+        xrandr --output "$ext" --primary --auto --right-of "$lap"
+    else
+        xrandr --output "$lap" --primary --auto
+    fi
+    xrandr --auto
+    ;;
+2)
+    xrandr --output "$lap" --off
+    ;;
+3)
+    xrandr --output "$ext" --scale 1 --same-as "$lap"
+    ;;
+*)
+    ;;
 esac
 hsetroot -fill ~/Downloads/private/daniel-leone-v7daTKlZzaw-unsplash.jpg
